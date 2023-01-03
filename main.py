5b053ff69fe9488d729a6f088babee40c2198dbb
sanaas246 <112659421+sanaas246@users.noreply.github.com>
2023-01-03 01:03:53 -0700

    1.03

--- main.py
+++ main.py
@@ -13,12 +13,14 @@ file.close()
 users = json.loads(user_from_file)
 
 # VARIABLES
+foundUserLi = [ ]
+
 
 
 
 # FUNCTIONS
 # Save to JSON
-def addhs(variable):
+def addtojson(variable):
     users.append(variable)
     users_json = json.dumps(users)
     file = open("users.txt", "w")
@@ -26,33 +28,36 @@ def addhs(variable):
     file.close()
 
 # Find the username or password
-def findInfo(info, item): 
-    for i in range(len(users)):
-        if users[i][info] == item:
+def findInfo(array, info, item): 
+    for i in range(len(array)):
+        if array[i][info] == item:
             return i
     return -1
 
+# Login and register menu functions 
 def register():
     username = input("\nWhat would you like your username to be? ")
     password = input("What would you like your password to be? ")
     depositsum = input("How much money is your account starting with? ")
     depositmonth = input("What month is it? ")
     deposityear = input("What year is it? ")
+    firstdep = [depositsum, depositmonth, deposityear]
     return {
         "username": username,
         "password": password,
-        "total": [(depositsum, depositmonth, deposityear)]
+        "total": firstdep
     }
 
 def login():
     usernameInput = input("Username: ")
-    foundUser = findInfo("username", usernameInput)
+    foundUser = findInfo(users, "username", usernameInput)
+    foundUserLi.append(foundUser)
     if foundUser != -1:
         passwordInput = input("Password: ")
-        foundPass = findInfo("password", passwordInput)
+        foundPass = findInfo(users, "password", passwordInput)
         if foundPass != -1:
             print("Correct password")
-            pass
+            mainmenu()
         else: 
             print("Wrong Password! Try again!")
             login()
@@ -64,18 +69,26 @@ def loginmenu():
     regOrLog = input("Would you like to register or login? ").lower() 
     if regOrLog == "login":
         login()
-        # mainmenu()
     elif regOrLog == "register":
         newuser = register()
-        addhs(newuser)
-        # mainmenu()
+        addtojson(newuser)
+        mainmenu()
 
+# Menu selection functions
+def displayall():
+    print("Deposit: ",users[foundUserLi[0]]["total"][0])
+    print("Date: ",users[foundUserLi[0]]["total"][1],",",users[foundUserLi[0]]["total"][2])
+
+def withdraw():
+    pass
+
+# Main menu functions 
 def getMenuSelection():
     # Menu Options
     print(f"\n********MAIN MENU********")
     print("1. Display all trasactions")
     print("2. Withdraw money")
-    print("3. Logout")
+    print("3. Exit")
     return input("\nChoose an option please: ").lower()
 
 def mainmenu():
@@ -88,10 +101,10 @@ def mainmenu():
         elif selection == "2" or selection == "withdraw money":
             withdraw()
         elif selection == "3" or selection == "exit":
-            exit()
             loop = False
+            print("Bye!")
         
-
+# Main Code Function
 def main():
     loginmenu()
 

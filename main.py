86eed6654190cb5c2ab270e8861b879f8c083df9
sanaas246 <112659421+sanaas246@users.noreply.github.com>
2023-01-02 19:00:21 -0700

    first

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
 # Finance Management by Sana S
+
+import json 
+
+# DICTIONARY
+# Users
+
+# Save users using JSON
+file = open("users.txt", "r")
+user_from_file = file.read()
+file.close()
+
+users = json.loads(user_from_file)
+
+def addhs(variable):
+    users_json = json.dumps(variable)
+    file = open("users.txt", "w")
+    file.write(users_json)
+    file.close()
+
+print(users)
